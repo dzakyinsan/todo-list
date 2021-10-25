4912ab0167e38b612fd68234a8251efffa521668
dzakyinsan20 <igabakardzaky@gmail.com>
2021-10-25 08:23:24 +0700

    minor

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
-import Logo from "./../assets/logo.png";
 import "./../App.css";
 
 function Header() {
   return (
     <div>
       <Navbar color="dark" light expand="md">
-        <NavbarBrand href="/">
-          <img src={Logo} className="brand-logo" alt="logo" />{" "}
-        </NavbarBrand>
+        <NavbarBrand href="/"></NavbarBrand>
       </Navbar>
     </div>
   );
